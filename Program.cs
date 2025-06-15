a0a044be50194fd460f91921ffdded8d6f52908d
Xola dos Santos <xola.dos.santos@accenture.com>
2025-06-15 14:18:26 +0200

    chore: add initial project files

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp
+{
+    public interface IAnimal
+    {
+        void makeSound();
+    }
+
+    public class Animal : IAnimal
+    {
+        public virtual void makeSound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Cat meows");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal myDog = new Dog();
+            IAnimal myCat = new Cat();
+
+            myDog.makeSound(); // Output: Dog barks
+            myCat.makeSound(); // Output: Cat meows
+
+            Animal genericAnimal = new Animal();
+            genericAnimal.makeSound(); // Output: Animal makes a sound
+        }
+    }
+}
